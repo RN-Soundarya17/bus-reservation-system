cf93eeda1366cb857dd196205c8d47718ef661fa
RN-Soundarya17 <rnsoundarya68@gmail.com>
2026-09-25 20:26:38 +0530

    Upload database connection file

---
+++ db_connection.php
@@ -0,0 +1,19 @@
+
+<?php
+
+$host = "localhost";
+$port = "3307";
+$username = "root";
+$password = "";
+$database = "bus_reservation";
+
+$conn = new mysqli($host, $username, $password, $database, $port);
+
+if ($conn->connect_error) {
+    die("Database connection failed: " . $conn->connect_error);
+}
+
+$conn->set_charset("utf8mb4");
+
+?>
+
